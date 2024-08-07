34d4d6f32001acaaac60e513c05b8011bfaa1b48
Blok-Dev1 <laubscher.stefan@gmail.com>
2024-08-08 00:20:07 +0200

    Added Zebra Bets

---
+++ src/Zebra.Bets/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Zebra.Bets;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("[1] for Home Team...");
+        Console.WriteLine("[2] for Visitor Team...");
+
+        var team = int.Parse(Console.ReadLine()?.TrimEnd('\r', '\n'));
+        
+    }
+}
